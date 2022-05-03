5f9936bad68c552386af4692b254e7c43c132568
Flutterish <CharmindYT@gmail.com>
2022-05-03 17:14:54 +0200

    add file drop support (in general)

--- OsuFrameworkDesigner/OsuFrameworkDesigner.Game/OsuFrameworkDesignerGameBase.cs
+++ OsuFrameworkDesigner/OsuFrameworkDesigner.Game/OsuFrameworkDesignerGameBase.cs
@@ -1,4 +1,5 @@
 using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
 using OsuFrameworkDesigner.Resources;
 
 namespace OsuFrameworkDesigner.Game;
@@ -18,4 +19,32 @@ public class OsuFrameworkDesignerGameBase : osu.Framework.Game {
 	private void load () {
 		Resources.AddStore( new DllResourceStore( typeof( OsuFrameworkDesignerResources ).Assembly ) );
 	}
+
+	public override void SetHost ( GameHost host ) {
+		base.SetHost( host );
+
+		if ( host.Window is OsuTKWindow tkWindow )
+			tkWindow.FileDrop += onTkWindowFileDrop;
+		else if ( host.Window is SDL2DesktopWindow sdlWindow )
+			sdlWindow.DragDrop += onSdlWindowDragDrop;
+	}
+
+	void onSdlWindowDragDrop ( string e ) {
+		FileDrop?.Invoke( new string[] { e } );
+	}
+
+	void onTkWindowFileDrop ( object? _, osuTK.Input.FileDropEventArgs e ) {
+		FileDrop?.Invoke( e.FileNames );
+	}
+
+	protected override void Dispose ( bool isDisposing ) {
+		base.Dispose( isDisposing );
+
+		if ( Host.Window is OsuTKWindow tkWindow )
+			tkWindow.FileDrop -= onTkWindowFileDrop;
+		else if ( Host.Window is SDL2DesktopWindow sdlWindow )
+			sdlWindow.DragDrop -= onSdlWindowDragDrop;
+	}
+
+	public event Action<string[]>? FileDrop;
 }
